0f1f88b36e2ccd500beb29d1b293008df18df233
Zioun <jionkhan0@gmail.com>
2024-04-29 07:07:41 +0600

    added .env

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nl88zl6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 console.log(uri);
 
@@ -24,56 +24,57 @@ async function run() {
   try {
     await client.connect();
 
-    const coffeeCollection = client.db("artAndCraftDB").collection("artAndCraft");
-    // const userCollection = client.db("coffeeDB").collection("user");
+    const artAndCraftCollection = client.db("artAndCraftDB").collection("artAndCraft");
 
     //get
-    // app.get("/addArtAndCraft", async (req, res) => {
-    //   const cursor = coffeeCollection.find();
-    //   const reslut = await cursor.toArray();
-    //   res.send(reslut);
-    // });
+    app.get("/artAndCraft", async (req, res) => {
+      const cursor = artAndCraftCollection.find();
+      const reslut = await cursor.toArray();
+      res.send(reslut);
+    });
     //get single item
-    // app.get("/coffee/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await coffeeCollection.findOne(query);
-    //   res.send(result);
-    // });
+    app.get("/artAndCraft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artAndCraftCollection.findOne(query);
+      res.send(result);
+    });
     //post
-    app.post("/addArtAndCraft", async (req, res) => {
+    app.post("/artAndCraft", async (req, res) => {
       const newCoffee = req.body;
       console.log(newCoffee);
-      const reslut = await coffeeCollection.insertOne(newCoffee);
+      const reslut = await artAndCraftCollection.insertOne(newCoffee);
       res.send(reslut);
     });
     //update
-    // app.put("/coffee/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const filter = { _id: new ObjectId(id) };
-    //   const options = { upsert: true };
-    //   const updatedCoffee = req.body;
-    //   const coffee = {
-    //     $set: {
-    //       name: updatedCoffee.name,
-    //       quantity: updatedCoffee.quantity,
-    //       supplier: updatedCoffee.supplier,
-    //       taste: updatedCoffee.taste,
-    //       category: updatedCoffee.category,
-    //       details: updatedCoffee.details,
-    //       photo: updatedCoffee.photo,
-    //     },
-    //   };
-    //   const result = await coffeeCollection.updateOne(filter, coffee, options);
-    //   res.send(result);
-    // });
+    app.put("/artAndCraft/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedArtAndCraft = req.body;
+      const coffee = {
+        $set: {
+          itemName: updatedArtAndCraft.itemName,
+          subCategory: updatedArtAndCraft.subCategory,
+          rating: updatedArtAndCraft.rating,
+          price: updatedArtAndCraft.price,
+          customization: updatedArtAndCraft.customization,
+          stockStatus: updatedArtAndCraft.stockStatus,
+          photoURL: updatedArtAndCraft.photoURL,
+          processingTime: updatedArtAndCraft.processingTime,
+          details: updatedArtAndCraft.details,
+        },
+      };
+      const result = await artAndCraftCollection.updateOne(filter, coffee, options);
+      res.send(result);
+    });
     //delete
-    // app.delete("/coffee/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = await coffeeCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    app.delete("/artAndCraft/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artAndCraftCollection.deleteOne(query);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
